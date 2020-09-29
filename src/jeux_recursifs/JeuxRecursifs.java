eacd88c067ca00a999360bca5a436f8e39096218
Paulin-Zumstein <paulin.zumstein@etu.umontpellier.fr>
2020-09-29 16:15:12 +0200

    commit

--- src/jeux_recursifs/JeuxRecursifs.java
+++ src/jeux_recursifs/JeuxRecursifs.java
@@ -1,4 +1,20 @@
 package jeux_recursifs;
 public class JeuxRecursifs {
 
+    public void resoudre(int n){
+
+    }
+
+    // à reprendre
+    public void resoudreAux(int n, int i, int j){
+        if (n==1){
+            System.out.println(i+"->"+j);
+        }else {
+            int k = 6-(i+j);
+            resoudreAux(n-1, i,k);
+            System.out.println(i+"->"+j);
+            resoudreAux(n-1,k,j);
+        }
+    }
+
 }
